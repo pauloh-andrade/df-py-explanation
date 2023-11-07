5ad7ac2bd588ed7d9b8b09a5c189297e5273b698
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-06 22:31:52 -0300

    feat: added statistcs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_excel('precos.xlsx')
 
@@ -6,8 +7,16 @@ df.head(10)
 
 list = df['Tipo'].unique()
 
-df_escola = df.query("Tipo=='Escola'")[["Tipo", "produto marca", "Preço"]]
+df_escola = df.query("Tipo=='Escola'")[["Tipo", "produto_marca", "Preço"]]
 
-media = df_escola.groupby(['Tipo', 'produto marca']).mean()
+media = df_escola.groupby(['Tipo', 'produto_marca']).agg({'Preço': ['mean', 'min', 'max', 'count']})
 
 print(media)
+
+esfiha = df.query("produto_marca=='Esfiha '")[['Estabelecimento', 'Preço']]
+
+print(esfiha)
+
+plt.figure()
+plt.bar(esfiha['Estabelecimento'], esfiha['Preço'], color='maroon')
+plt.show()
