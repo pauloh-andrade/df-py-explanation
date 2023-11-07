be8708c5d83c68af5dd21a4ad94ba8627b6b45d2
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-06 22:47:07 -0300

    feat: added dashboard

---
+++ dashboard.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.read_excel('precos.xlsx')
+
+st.set_page_config(layout='wide')
+st.title('Estatísticas de preços')
+
+(col1, col2) = st.columns(2)
+
+with col1:
+    lista = df['Tipo'].unique()
+    tipo = st.selectbox('Tipo', lista)
+
+    df_tipo = df.query(f"Tipo=='{tipo}'")
+
+    df_tipo[['Coleta', 'Tipo', 'Semana', 'Estabelecimento', 'produto_marca', 'Preço']]
